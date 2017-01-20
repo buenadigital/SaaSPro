1fac266a16f34d1b4412f765e8a94c3385ea1082
todd mueller <tmueller@buenadigital.com>
2017-01-20 11:00:57 -0800

    SendGrid Updated
    changes made to credentials

--- src/SaasPro.TestsConfiguration/TestsAppSettings.cs
+++ src/SaasPro.TestsConfiguration/TestsAppSettings.cs
@@ -4,13 +4,13 @@ namespace SaasPro.TestsConfiguration
 {
 	public class TestsAppSettings : IApplicationSettings
 	{
-        public string APIKey => "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";
+        public string APIKey => "XXXX";
 		public string MailServer => "smtp.sendgrid.net";
-		public string MailUserName => "buenadigital";
-		public string MailPassword => "!Password##9";
+		public string MailUserName => "XXXX";
+		public string MailPassword => "XXX";
 		public bool MailSSL => true;
 		public bool SendEmail => true;
-		public string SendEmailTo => "tmueller@buenadigital.com";
+		public string SendEmailTo => "XXXX";
 		public bool EnableOptimizations => false;
 	}
 }
